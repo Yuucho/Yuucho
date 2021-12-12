500cdb0db28786c199b0d6477a46aaaea14fa01d
bluking <92104566+bluking@users.noreply.github.com>
2021-12-12 17:54:46 +0800

    Add files via upload
    通讯录

--- test.c
+++ test.c
@@ -1,78 +1,78 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include "SList.h"
 
-void TestSList1()
+
+#include "contact.h"
+
+void menu()
 {
-	SLTNode *plist = NULL;
-	SListPushBack(&plist, 1);
-	SListPushBack(&plist, 2);
-	SListPushBack(&plist, 3);
-	SListPushFront(&plist, 0);
-	SListPrint(plist);
-	SListPopFront(&plist);
-	SListPopFront(&plist);
-	SListPrint(plist);
-	SListPopFront(&plist);
-	SListPopFront(&plist);
-	SListPopFront(&plist);
-	SListPrint(plist);
+	printf("******************************\n");
+	printf("****  1. add     2. del  *****\n");
+	printf("****  3. search  4. modify****\n");
+	printf("****  5. show    6. sort   ***\n");
+	printf("****  0. exit               **\n");
+	printf("******************************\n");
 }
 
-void TestSLis2()
+enum Option
 {
-	SLTNode *plist = NULL;
-	SListPushBack(&plist, 1);
-	SListPushBack(&plist, 2);
-	SListPushBack(&plist, 3);
-	SListPushBack(&plist, 4);
-	SListPrint(plist);
-	SListPopBack(&plist);
-	SListPopBack(&plist);
-	SListPopBack(&plist);
-	SListPopBack(&plist);
-	SListPopBack(&plist);
-	SListPrint(plist);
-}
-void TestSLis3()
-{
-	SLTNode *plist = NULL;
-	SListPushBack(&plist, 1);
-	SListPushBack(&plist, 2);
-	SListPushBack(&plist, 3);
-	SListPushBack(&plist, 4);
-	SListPrint(plist);
-	SLTNode*pos = SListFind(plist, 1);
-	if (pos)
-	{
-		SListInsert(&plist, pos, 30);
-	}
-	SListPrint(plist);
-}
-void TestSLis4()
-{
-	SLTNode *plist = NULL;
-	SListPushBack(&plist, 1);
-	SListPushBack(&plist, 2);
-	SListPushBack(&plist, 3);
-	SListPushBack(&plist, 4);
-	SListPrint(plist);
-	SLTNode*pos = SListFind(plist, 1);
-	if (pos)
-	{
-		SListErase(&plist, pos);
-	}
-	SListPrint(plist);
+	EXIT,
+	ADD,
+	DEL,
+	SEARCH,
+	MODIFY,
+	SHOW,
+	SORT
+};
 
-	pos = SListFind(plist, 4);
-	if (pos)
-	{
-		SListErase(&plist, pos);
-	}
-	SListPrint(plist);
-}
 int main()
 {
-	TestSLis4();
+	int input = 0;
+	//创建一个通讯录
+	struct Contact con;
+	//初始化通讯录
+	InitContact(&con);
+	//最多可以放3个人的信息了
+	//空间不够可以增容
 
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case EXIT:
+			//保存信息
+			SaveContact(&con);
+			//销毁通讯录
+			DestroyContact(&con);
+			printf("退出通讯录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
 	return 0;
 }
+
+//文件的版本
+//1. 退出通讯录的时候，保存信息到文件中
+//2. 运行起来通讯录的时候，加载文件中的信息到通讯录中
+
+
